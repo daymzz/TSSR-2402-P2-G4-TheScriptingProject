f8e5c5f2aa5f7e146a4f92631f7895abea6c5f6e
Kamalors <162970946+Kamalors@users.noreply.github.com>
2024-04-19 11:01:11 +0200

    Update Menu_PowerShell.ps1

--- Script/Menu_PowerShell.ps1
+++ Script/Menu_PowerShell.ps1
@@ -157,81 +157,71 @@ function ActionSysteme {
 
 }
 
+function createDossier {
+    param(
+        [string]$createDir,
+        [string]$pathDir
+    )
+    try {
+        New-Item -Path "$pathDir\$createDir" -ItemType Directory -ErrorAction SilentlyContinue
+        Write-Host "Répertoire $createDir créé à $pathDir"
+    } catch {
+        Write-Host "Erreur, vérifier que le chemin soit bien saisi."
+    }
+}
+
+function modifyDossier {
+    param(
+        [string]$modifyDir,
+        [string]$pathModifyDir,
+        [string]$newNameDir
+    )
+    try {
+        Rename-Item -Path "$pathModifyDir\$modifyDir" -NewName $newNameDir
+        Write-Host "Répertoire $modifyDir a été modifié en $newNameDir à $pathModifyDir"
+    } catch {
+        Write-Host "Erreur, vérifier que le chemin soit bien saisi."
+    }
+}
+
+function deleteDossier {
+    param(
+        [string]$suppDir,
+        [string]$pathSuppDir
+    )
+    try {
+        Remove-Item -Path "$pathSuppDir\$suppDir" -Recurse -Force
+        Write-Host "Répertoire $suppDir est supprimé de $pathSuppDir"
+    } catch {
+        Write-Host "Erreur, vérifier que le chemin soit bien saisi."
+    }
+}
+
 function ActionRepertoire {
     while ($true) {
-        function createDossier {
-            param(
-                [string]$createDir,
-                [string]$pathDir
-            )
-            try {
-                New-Item -Path "$pathDir\createDir" -ItemType Directory -ErrorAction SilentlyContinue
-                Write-Host "Répertoire $createDir créé"
-            }
-            catch {
-                Write-Host "erreur, verifier que le chemin soit bien saisi"
-                return
-            }
-            $pathDir = Read-Host "Veuillez saisir un chemin :"
-            $createDir = Read-Host "Veuillez saisir le nom du dossier a créer :"
-            #modifier dossier
-        }
-        function modifyDossier {
-            param(
-                [string]$modifyDir,
-                [string]$pathModifyDir,
-                [string]$newNameDir
-            )
-            try {
-                Rename-Item -Path "$pathModifyDir$modifyDir" $newNameDir
-                Write-Host "Répertoire $modifyDir a été modifié en $newName"
-            }
-            catch {
-                Write-Host "erreur, verifier que le chemin soit bien saisi"
-                return
-            }
-            $pathModifyDir = Read-Host "Veuillez saisir un chemin :"
-            $modifyDir = Read-Host "Veuillez saisir le nom du dossier a modifer :"
-            $newNameDir = Read-Host "Veuillez saisir le nouveau nom :"
-        }
-        function deleteDossier {
-            param(
-                [string]$suppDir,
-                [string]$pathSuppDir
-            )
-            try {
-                Remove-Item -Path "$pathSuppDir$suppDir" -Recurse -Force
-                Write-Host "Répertoire $suppDir est supprimé"
-            }
-            catch {
-                Write-Host "erreur, verifier que le chemin soit bien saisi"
-                return
-            }
-            $pathSuppDir = Read-Host "Veuillez saisir un chemin :"
-            $suppDir = Read-Host "Veuillez saisir le nom du dossier a supprimer :"
-        }
-
-
-        # Affiche un menu 
         Clear-Host
-        Write-Output ""
-        Write-Output "1 - création répertoire"
-        Write-Output "2 - Duppression de répertoire  "
-        Write-Output "3 - modifier un repertoire"
-        Write-Output "4 - retrour "
+        Write-Output "1 - Création de répertoire"
+        Write-Output "2 - Suppression de répertoire"
+        Write-Output "3 - Modifier un répertoire"
+        Write-Output "4 - Retour"
         $action = Read-Host "Entrez le numéro de votre choix"
+        
         switch ($action) {
             "1" {
-                # création répertoire
-                Invoke-Command -ComputerName $iPDistant -ScriptBlock {createDossier } -Credential $Credential
+                $pathDir = Read-Host "Veuillez saisir un chemin :"
+                $createDir = Read-Host "Veuillez saisir le nom du dossier à créer :"
+                Invoke-Command -ComputerName $iPDistant -ScriptBlock ${function:createDossier} -ArgumentList $createDir, $pathDir -Credential $Credential
             }
             "2" {
-                # suppression de répertoire :
-                Invoke-Command -ComputerName $iPDistant -ScriptBlock { deleteDossier} -Credential $Credential
+                $pathSuppDir = Read-Host "Veuillez saisir un chemin :"
+                $suppDir = Read-Host "Veuillez saisir le nom du dossier à supprimer :"
+                Invoke-Command -ComputerName $iPDistant -ScriptBlock ${function:deleteDossier} -ArgumentList $suppDir, $pathSuppDir -Credential $Credential
             }
             "3" {
-                # modifier un repertoire :
-                Invoke-Command -ComputerName $iPDistant -ScriptBlock { modifyDossier} -Credential $Credential
+                $pathModifyDir = Read-Host "Veuillez saisir un chemin :"
+                $modifyDir = Read-Host "Veuillez saisir le nom du dossier à modifier :"
+                $newNameDir = Read-Host "Veuillez saisir le nouveau nom :"
+                Invoke-Command -ComputerName $iPDistant -ScriptBlock ${function:modifyDossier} -ArgumentList $modifyDir, $pathModifyDir, $newNameDir -Credential $Credential
             }
             "4" {
                 Write-Output "Fin du script."
@@ -241,9 +231,9 @@ function ActionRepertoire {
                 Write-Output "Action non reconnue. Veuillez essayer à nouveau."
             }
         }
+    }
 }
 
-}
 
 function ActionSecurite {
     Clear-Host
@@ -275,62 +265,49 @@ function ActionSecurite {
     } 
 }
 
+
+
+# Définition des fonctions principales d'installation et de désinstallation
+function Installer {
+    param (
+        [string]$nameInstallApp
+    )
+    try {
+        Start-Process -FilePath $nameInstallApp -Wait
+        Write-Host "Installation réussie du logiciel: $nameInstallApp"
+    } catch {
+        Write-Host "Erreur! Veuillez vérifier que le fichier et le chemin existent ou qu'ils soient écrits de la bonne manière."
+    }
+}
+
+function Desinstaller {
+    param (
+        [string]$nameUninstallApp
+    )
+    try {
+        Start-Process -FilePath $nameUninstallApp -Wait
+        Write-Host "Désinstallation réussie du logiciel: $nameUninstallApp"
+    } catch {
+        Write-Host "Erreur! Veuillez vérifier que le fichier et le chemin existent ou qu'ils soient écrits de la bonne manière."
+    }
+}
+
 function ActionLogiciel {
-        # Demande l'adresse IP distant
-    $iPDistant = Read-Host "Ip ordinateur distant"
-
-    # Demande les informations Credential (c'est le nom du compte d'en face"
-    $Credential = Get-Credential
-
-    function Installer {
-        function installSoftware {
-            param (
-                [string]$nameInstallApp
-            )
-            try{
-                start-Process -FilePath "$nameInstallApp"
-            }catch{
-                Write-Host "Erreur! Veuillez verifier que le fichier et le chemin existe ou qu'il soient ecrit de la bonne maniere. "
-            }
-        }
-        
-        $nameInstallApp = Read-Host "Entre le chemin complete suivi du noms de fichier installation"
-        installSoftware $nameInstallApp
-    }
-
-    function Désinstaller {
-        function uninstallSoftware {
-            param (
-                [string]$nameUninstallApp
-            )
-            try{
-                start-Process -FilePath "$nameUninstallApp"
-            }catch{
-                Write-Host "Erreur! Veuillez verifier que le fichier et le chemin existe ou qu'il soient ecrit de la bonne maniere. "
-            }
-        }
-        
-        $nameUninstallApp = Read-Host "Entre le chemin complete suivi du noms de fichier desinstallation"
-        uninstallSoftware $nameUninstallApp
-        
-    }
     while ($true) {
-        # Affiche un menu 
         Clear-Host
-        Write-Output ""
-        Write-Output "1 - Installer "
-        Write-Output "2 - Désinstaller  "
-        Write-Output "3 - Retour menu"
+        Write-Output "1 - Installer"
+        Write-Output "2 - Désinstaller"
+        Write-Output "3 - Retour au menu principal"
         $action = Read-Host "Entrez le numéro de votre choix"
 
         switch ($action) {
             "1" {
-                # Arrêt  distant
-                Invoke-Command -ComputerName $iPDistant -ScriptBlock { Installer} -Credential $Credential
+                $nameInstallApp = Read-Host "Entrez le chemin complet suivi du nom du fichier d'installation"
+                Invoke-Command -ComputerName $iPDistant -ScriptBlock ${function:Installer} -ArgumentList $nameInstallApp -Credential $Credential
             }
             "2" {
-                # Redémarrage distant
-                Invoke-Command -ComputerName $iPDistant -ScriptBlock { Désinstaller } -Credential $Credential
+                $nameUninstallApp = Read-Host "Entrez le chemin complet suivi du nom du fichier de désinstallation"
+                Invoke-Command -ComputerName $iPDistant -ScriptBlock ${function:Desinstaller} -ArgumentList $nameUninstallApp -Credential $Credential
             }
             "3" {
                 Write-Output "Fin du script."
@@ -340,17 +317,14 @@ function ActionLogiciel {
                 Write-Output "Action non reconnue. Veuillez essayer à nouveau."
             }
         }
+    }
 }
 
-}
 
 function ActionBureauADistance {
     
 }
 
-function Information-Utilisateur {
-    
-}
 
 function Information-Systeme {
         #Demande l'adresse IP distant
@@ -403,71 +377,88 @@ function Information-Systeme {
             "1" {
             #   Liste des sessions ouvertes par l'utilisateur :
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-localUser -name wilder | Select-Object Enabled  } -Credential $Credential
+                Sleep 5
             }
             "2" {
                 # Groupe d’appartenance d’un utilisateur :
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-localUser -name wilder | Select-Object ObjectClass } -Credential $Credential
+                Sleep 5
             }
             "3" {
-            # date de derniere connexion d'un utilisateur
+            # date de derniere conneSleep 5ion d'un utilisateur
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock {Get-localUser -name wilder | Select-Object LastLogon } -Credential $Credential
+                Sleep 5
             }
             "4" {
-                #    Historique des commandes exécutées par l'utilisateur :
+                #    Historique des commandes eSleep 5écutées par l'utilisateur :
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-History} -Credential $Credential
+                Sleep 5
             }
             "5" {
                 #   Droits/permissions de l’utilisateur sur un dossier : a revoir
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-Acl -Path "Chemin\Vers\Dossier" | Format-List } -Credential $Credential
+                Sleep 5
             }
             "6" {
                 #   Droits/permissions de l’utilisateur sur un fichier :
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-Acl -Path "Chemin\Vers\Fichier" | Format-List
             } -Credential $Credential
+            Sleep 5
             }
             "7" {
                 #   Version de l'OS : ok
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-Computer OsName, OsVersion} -Credential $Credential
+                Sleep 5
             }
             "8" {
                 #   Nombre de disques : ok
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-Disk | Measure-Object | Select-Object Count} -Credential $Credential
+                Sleep 5
             }
             "9" {
-                #   Partition (nombre, nom, FS, taille) par disque : ok a voir si y a mieux
+                #   Partition (nombre, nom, FS, taille) par disque : ok a voir si y a mieuSleep 5
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-Partition | Select-Object DiskNumber, Name, FileSystem, Size } -Credential $Credential
+                Sleep 5
             }
             "10" {
                 #   Espace disque restant par partition/volume : ok
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-Volume | Select-Object DriveLetter, FileSystemLabel, SizeRemaining } -Credential $Credential
+                Sleep 5
             }
             "11" {
             #   Nom et espace disque d'un dossier (nom de dossier demandé) :
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { infoDossier } -Credential $Credential
+                Sleep 5
             }
             "12" {
                 #   Liste des lecteurs montés (disque, CD, etc.) :ok
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-PSDrive | Where-Object {$_.Provider -match "FileSystem"}} -Credential $Credential
+                Sleep 5
             }
             "13" {
             #  Nombre d'interfaces : ok
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-NetAdapter | Measure-Object | Select-Object Count} -Credential $Credential
+                Sleep 5
             }
             "14" {
             #   Adresse IP de chaque interface : ok
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-NetIPAddress | Select-Object InterfaceAlias, IPAddress} -Credential $Credential
+                Sleep 5
             }
             "15" {
             #   Adresse Mac : ok
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-NetAdapter | Select-Object Name, MacAddress} -Credential $Credential
+                Sleep 5
             }
             "16" {
                 #   Liste des ports ouverts : ok
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-NetTCPConnection | Where-Object {$_.State -eq "Listen"}} -Credential $Credential
+                Sleep 5
             }
             "17" {
             #    Statut du pare-feu : ok
                 Invoke-Command -ComputerName $iPDistant -ScriptBlock { Get-NetFirewallProfile | Select-Object Name, Enabled} -Credential $Credential
+                Sleep 5
             }
             "0" {
                 Write-Output "Fin du script."
